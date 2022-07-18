8cf16f256230da364906b52b4419430bb9ef7fab
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-07-18 22:41:51 +0700

    create pages & routes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,16 @@
-
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";	
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxmAsO052l0Q2gRiohRa7okZIRWDokJEs",
-  authDomain: "house-marketplace-app-11e94.firebaseapp.com",
-  projectId: "house-marketplace-app-11e94",
-  storageBucket: "house-marketplace-app-11e94.appspot.com",
-  messagingSenderId: "558158153202",
-  appId: "1:558158153202:web:2c6658bc82df13ae95576b"
-};
+  apiKey: 'AIzaSyDA8LVcBB6ZuFMGtZZLEh_veJ44WGrNRdE',
+  authDomain: 'house-marketplace-app-fb1d0.firebaseapp.com',
+  projectId: 'house-marketplace-app-fb1d0',
+  storageBucket: 'house-marketplace-app-fb1d0.appspot.com',
+  messagingSenderId: '832068369979',
+  appId: '1:832068369979:web:dce177da9bfc60a4b4e61e',
+}
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+initializeApp(firebaseConfig)
 export const db = getFirestore()
